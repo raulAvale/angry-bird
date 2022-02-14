e3c0c4cfd9c1807398e31d071a5d0c7c84ea5b48
raulAvale <94195203+raulAvale@users.noreply.github.com>
2022-02-14 16:03:44 -0300

    Add files via upload

---
+++ Angry-Bird-Etapa-6-main/Angry-Bird-Etapa-6-main/Bird.js
@@ -0,0 +1,23 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smoke = loadImage("sprites/smoke.png");
+    this.trajetoria = []
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+    if(this.body.position.x > 200&&this.body.velocity.x > 10){
+var position = [this.body.position.x,this.body.position.y]
+this.trajetoria.push(position);
+    }
+    for (let index = 0; index < this.trajetoria.length; index++) {
+      image(this.smoke,this.trajetoria[index][0],this.trajetoria[index][1]);
+      
+    }
+  }
+}
